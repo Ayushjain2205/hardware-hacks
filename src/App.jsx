40470518880f5323e7569d2e87b9d7bbc628f2f9
Ayush Jain <ayushjain2205@gmail.com>
2023-04-08 23:47:41 +0530

    Setup modals

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import { useState } from "react";
+import Sheet from "react-modal-sheet";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [isOpen, setOpen] = useState(false);
 
   return (
     <div className="App">
       <button class="btn btn-primary">Button</button>
+      <button onClick={() => setOpen(true)}>Open sheet</button>
+
+      <Sheet isOpen={isOpen} onClose={() => setOpen(false)}>
+        <Sheet.Container>
+          <Sheet.Header />
+          <Sheet.Content>{/* Your sheet content goes here */}</Sheet.Content>
+        </Sheet.Container>
+
+        <Sheet.Backdrop />
+      </Sheet>
     </div>
   );
 }
